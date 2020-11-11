800949fb6e76ca0567a2494ba7c8943d5380f3ba
dFuse_mArv <marvin.sid03@gmail.com>
2020-11-11 08:43:19 -0800

    'adding_form_hooks'

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,80 @@
 import React, { useState } from 'react';
+//import ReactDom from 'react-dom';
 
-const App=(props)=>{
-    const stateName=useState(()=>{
-        return '';
+const App=()=>{
+    const state=useState({
+       fname: '',
+       lname: '',
+       mail: '',
+       mobile: '',
     });
 
-    const [fullName,setFullName]=useState();
-    const [currName,setName]=stateName;
+    const [valuee,updateValue]=state;
 
-    const InputHandler=(event)=>{
-        setName(event.target.value);
-    };
+    const inputEvent=(event)=>{
+        console.log(event.target.value);
+        console.log(event.target.name);
+        const value=event.target.value;
+        const name=event.target.name;
+        
+        updateValue((prevValue)=>{
+            //console.log(prevValue);
+            if(name==='fname'){
+                return{
+                fname: value,
+                lname:  prevValue.lname,
+                mail:  prevValue.mail,
+                mobile:  prevValue.mobile,
+            };
+                
+            }else if(name==='lname'){
+                return{
+                    fname: prevValue.fname,
+                lname:  value,
+                mail:  prevValue.mail,
+                mobile:  prevValue.mobile,
+                };
+            }else if(name==='mail'){
+                return{
+                    fname: prevValue.fname,
+                lname:  prevValue.lname,
+                mail:  value,
+                mobile:  prevValue.mobile,
+                };
+            }else if(name==='mobile'){
+                return{
+                    fname: prevValue.fname,
+                lname:  prevValue.lname,
+                mail:  prevValue.mail,
+                mobile:  value,
+                };
+            }
+        });
 
-    const submission=()=>{
-        setFullName(currName);
-    };
+    }
 
-    
+
+    const onSubmits=(event)=>{
+        event.preventDefault();
+        alert('form submitted!');
+    }
     return(
         <>
-            <h1>Hello! {fullName}</h1>
-            <input type='text' placeholder='Enter your name' onChange={InputHandler} value={currName} />
-            <br />
-            <button onClick={submission}>Submit</button>
+        <form onSubmit={onSubmits}>
+            <h1>Hello {valuee.fname} {valuee.lname}</h1>
+            <h2>{valuee.mail}</h2>
+            <h3>{valuee.mobile}</h3>
+
+            <input type='text' placeholder='Enter your first name here' 
+                name='fname' onChange={inputEvent} /><br /><br />
+            <input type='text' placeholder='Enter your last name here' 
+                name='lname' onChange={inputEvent} /><br /><br />
+            <input type='email' name='mail' 
+                onChange={inputEvent} /><br /><br />
+            <input type='text' name='mobile' 
+                onChange={inputEvent} /><br /><br /> 
+            <button>Submit</button>
+        </form>
         </>
     );
 }
